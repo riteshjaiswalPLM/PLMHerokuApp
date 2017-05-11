410f7c6bbbeee196a923da0ef97e02dccf12bd5e
dhruv.javiya <dhruv.javiya@DTSU050.sailfin.com>
2017-05-11 18:58:06 +0530

    commit config index

--- config/index.js
+++ config/index.js
@@ -13,19 +13,36 @@ if(!global.hasOwnProperty('config')){
             environment: 'PRODUCTION'
         },
         demo_sfdc: {
-            username: 'admin@esm.demo3',
+           username: 'admin@esm.demo3',
             password: 'akritiv*1234',
             token: 'q5HveDEOSkyobijMVunvpEoE',
             environment: 'PRODUCTION'
+
         },
         smtpconfig :{
-                host: 'smtp.gmail.com',
-                port: 587,
-                secure: false, // use SSL
-                auth: {
-                    user: 'customer.support@akritiv.com',
-                    pass: '@kritiv*support'
-                }
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false, // use SSL
+            auth: {
+                user: 'customer.support@akritiv.com',
+                pass: '@kritiv*support'
+            }
+        },
+        dashboardConfig :{
+            title: 'My Task',
+            tabLabel: 'Home',
+            icon: 'fa fa-dashboard',
+            tabICON: 'fa fa-dashboard',
+            active: true,
+            showRefreshResult: true
+        },
+        archivalConfig :{
+            title: 'Archival',
+            tabLabel: 'Archival',
+            icon: 'fa fa-archive',
+            tabICON: 'fa fa-archive',
+            active: true,
+            showRefreshResult: true
         }
     };
 }
