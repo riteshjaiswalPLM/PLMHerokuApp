0159a149a5ac7de3b70e924ce9e0d3934b90f823
dhruv.javiya <dhruv.javiya@DTSU050.sailfin.com>
2017-05-16 11:22:26 +0530

    Table Fix Heade changes

--- public/bower_components/angu-fixed-header-table/angu-fixed-header-table.js
+++ public/bower_components/angu-fixed-header-table/angu-fixed-header-table.js
@@ -6,9 +6,11 @@
 (function () {
     angular
         .module('anguFixedHeaderTable', [])
-        .directive('fixedHeader', fixedHeader);
+        .directive('fixedHeader', fixedHeader)
+        .directive('fixedHeaderSearchTable', fixedHeaderSearchTable);
 
     fixedHeader.$inject = ['$timeout'];
+    fixedHeaderSearchTable.$inject = ['$timeout'];
 
     function fixedHeader($timeout) {
         return {
@@ -74,6 +76,80 @@
                         scrollBarWidth -= 2;
                         var lastColumn = elem.querySelector('tbody tr:first-child td:last-child');
                         lastColumn.style.width = (lastColumn.offsetWidth - scrollBarWidth) + 'px';
+                    }
+                });
+            }
+        }
+    }
+
+    function fixedHeaderSearchTable($timeout) {
+        return {
+            restrict: 'A',
+            link: link
+        };
+
+        function link($scope, $elem, $attrs, $ctrl) {
+            var elem = $elem[0];
+            $scope.$watch(tableDataLoaded, function(isTableDataLoaded) {
+                if (isTableDataLoaded) transformTable();
+            });
+
+            function tableDataLoaded() {
+                var firstCell = elem.querySelector('tbody tr:first-child td:first-child');
+                return firstCell && !firstCell.style.width;
+            }
+
+            function transformTable() {
+                angular.element(elem.querySelectorAll('thead, tbody, tfoot')).css('display', '');
+                
+                $timeout(function () {
+                    // set widths of columns
+                    if(elem.querySelectorAll('tr:first-child th').length < 10){
+                        angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
+
+                            var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
+                            var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
+
+                            var columnWidth = tdElems ? tdElems.offsetWidth : thElem.offsetWidth;
+                            if (tdElems) {
+                                tdElems.style.width = columnWidth + 'px';
+                            }
+                            if (thElem) {
+                                thElem.style.width = columnWidth + 'px';
+                            }
+                            if (tfElems) {
+                                tfElems.style.width = columnWidth + 'px';
+                            }
+                        });
+                    }
+
+                    // set css styles on thead and tbody
+                    angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
+
+                    angular.element(elem.querySelectorAll('tbody')).css({
+                        'display': 'block',
+                        'height': $attrs.tableHeight || 'inherit',
+                        'overflow': 'auto'
+                    });
+
+                    // reduce width of last column by width of scrollbar
+                    var tbody = elem.querySelector('tbody');
+                    var scrollBarWidth = tbody.offsetWidth - tbody.clientWidth;
+                    if (scrollBarWidth > 0) {
+                        // for some reason trimming the width by 2px lines everything up better
+                        scrollBarWidth -= 2;
+                        var lastColumn = elem.querySelector('tbody tr:first-child td:last-child');
+                        lastColumn.style.width = (lastColumn.offsetWidth - scrollBarWidth) + 'px';
+                        if(elem.querySelectorAll('tr:first-child th').length > 9){
+                            angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
+                                var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
+                                var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
+                                var width = (thElem.offsetWidth < tdElems.offsetWidth ? tdElems.offsetWidth : thElem.offsetWidth);
+                                if (thElem) {thElem.style.minWidth = width + 'px',thElem.style.maxWidth = width + 'px'};
+                                if (tdElems) {tdElems.style.minWidth = width + 'px';tdElems.style.maxWidth = width + 'px'};
+                                if (tfElems) {tfElems.style.minWidth = width + 'px';tfElems.style.maxWidth = width + 'px'};
+                            });
+                        }
                     }
                 });
             }
