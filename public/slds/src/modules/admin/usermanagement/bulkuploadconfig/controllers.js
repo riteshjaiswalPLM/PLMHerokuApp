14eee3f5393755e295a87b7fdf4c040e1c4325d4
sonam.patel <sonam.patel@LTSU016.sailfin.com>
2017-06-23 18:46:19 +0530

    ESMPROD-501 - Create user upload module

--- public/slds/src/modules/admin/usermanagement/bulkuploadconfig/controllers.js
+++ public/slds/src/modules/admin/usermanagement/bulkuploadconfig/controllers.js
@@ -279,7 +279,7 @@ admin.controller('AdminUserManageBulkUploadConfigController', [
 
             if (isValid && configs.length > 0) {
                 if (!isUsernameFieldConfigured) {
-                    $dialog.alert("Please select your Unique field.");
+                    $dialog.alert("Please select Unique field.");
                 }
                 else {
                     if (!$scope.blockUI.userFieldsMapping.state().blocking && $scope.section.columns[0].length > 0) {
