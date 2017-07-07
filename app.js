7bc914d0e68de846881100bc31fa6ded380dc5e0
dhruv.javiya <dhruv.javiya@DTSU050.sailfin.com>
2017-07-07 16:42:13 +0530

    Changes Regarding redirect http request to https

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var favicon         = require('serve-favicon');
 var logger          = require('morgan');
 var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
+var sslRedirect     = require('heroku-ssl-redirect');
 
 var app = express();
+
+// enable ssl redirect
+app.use(sslRedirect());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
