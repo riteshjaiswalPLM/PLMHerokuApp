60f2d44c504d095ca532b6f1cfa6cd03843ad928
sonam.patel <sonam.patel@LTSU016.sailfin.com>
2017-07-27 11:25:51 +0530

    ESMPROD-707 - ESM Product (QA - Heroku Portal) : Layouts : When move fields on layout it display twice - change revert back

--- public/slds/src/modules/admin/mobile/layout/controllers.js
+++ public/slds/src/modules/admin/mobile/layout/controllers.js
@@ -252,7 +252,7 @@ admin.controller('AdminMobileLayoutsEditController',[
         $scope.removeAndReorder = function(items,item,index){
             var subRemoveAndReoprder = function(items,item,index){
                 item.deleted = true;
-                if(item.id === undefined || item.type == "Search-Criteria-Field" || item.type == "Search-Result-Field"){
+                if(item.id === undefined){
                     items.splice(index,1);
                 }
                 
