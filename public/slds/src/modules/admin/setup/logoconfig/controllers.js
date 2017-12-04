ce54560730ca8e9ec75828f49b1781febeb8ff29
sonam.patel <sonam.patel@LTSU016.sailfin.com>
2017-12-04 11:46:12 +0530

    ESMPROD-869 - [Mitie] - Custom Enhancements in Heroku Application - file size validation in logo upload

--- public/slds/src/modules/admin/setup/logoconfig/controllers.js
+++ public/slds/src/modules/admin/setup/logoconfig/controllers.js
@@ -7,54 +7,58 @@ admin.controller('AdminSetupLogoConfigController',['$scope','Upload','$rootScope
         if(files.length>0){
             file=files[0];
         }
-        if(file !=undefined && (($scope.allowedExtList.indexOf(file.name.toLowerCase().substr(file.name.indexOf("."),file.name.length - 1)) > -1) ))
+        if(file !=undefined && (($scope.allowedExtList.indexOf(file.name.toLowerCase().substr(file.name.lastIndexOf("."),file.name.length - 1)) > -1) ))
         {
-            
-            if(forLogo=='Home'){
-                if(!$scope.blockUI.loadOrgDetail.state().blocking){
-                    $scope.blockUI.loadOrgDetail.start('Uploading Home Page Logo...');
-                    Upload.upload({
-                        url: '/api/admin/setup/logoconfig/homelogo',
-                        data: {file: file}
-                    }).then(function(response){
-                        if(response.data.success)
-                        {
-                            $scope.blockUI.loadOrgDetail.stop();
-                            // $dialog.alert('Logo Uploaded Successfully');
-                            $scope.tempHomeLogoImg=$scope.homeLogoImg+new Date().getTime();
-                        }
-                        else
-                        {
-                            $dialog.alert(file.name + ' upload failed.','Error','pficon pficon-error-circle-o');
-                        }
-                    });
-                }
+            if (file.size > 102400) {
+                $dialog.alert('Maximum 100 KB file size is allowed.', 'Error', 'pficon pficon-error-circle-o');
             }
-            else if(forLogo=='Header'){
-                if(!$scope.blockUI.loadOrgDetail.state().blocking){
-                    $scope.blockUI.loadOrgDetail.start('Uploading Header Logo...');
-                    Upload.upload({
-                        url: '/api/admin/setup/logoconfig/headerlogo',
-                        data: {file: file}
-                    }).then(function(response){
-                        if(response.data.success)
-                        {
-                            $scope.blockUI.loadOrgDetail.stop();
-                            // $dialog.alert('Logo Uploaded Successfully');
-                             $scope.tempHeaderLogoImg=$scope.headerLogoImg+new Date().getTime();;
-                             
-                        }
-                        else
-                        {
-                            $dialog.alert(file.name + ' upload failed.','Error','pficon pficon-error-circle-o');
-                        }
-                    });
+            else {
+                if(forLogo=='Home'){
+                    if(!$scope.blockUI.loadOrgDetail.state().blocking){
+                        $scope.blockUI.loadOrgDetail.start('Uploading Home Page Logo...');
+                        Upload.upload({
+                            url: '/api/admin/setup/logoconfig/homelogo',
+                            data: {file: file}
+                        }).then(function(response){
+                            if(response.data.success)
+                            {
+                                $scope.blockUI.loadOrgDetail.stop();
+                                // $dialog.alert('Logo Uploaded Successfully');
+                                $scope.tempHomeLogoImg=$scope.homeLogoImg+new Date().getTime();
+                            }
+                            else
+                            {
+                                $dialog.alert(file.name + ' upload failed.','Error','pficon pficon-error-circle-o');
+                            }
+                        });
+                    }
+                }
+                else if(forLogo=='Header'){
+                    if(!$scope.blockUI.loadOrgDetail.state().blocking){
+                        $scope.blockUI.loadOrgDetail.start('Uploading Header Logo...');
+                        Upload.upload({
+                            url: '/api/admin/setup/logoconfig/headerlogo',
+                            data: {file: file}
+                        }).then(function(response){
+                            if(response.data.success)
+                            {
+                                $scope.blockUI.loadOrgDetail.stop();
+                                // $dialog.alert('Logo Uploaded Successfully');
+                                $scope.tempHeaderLogoImg=$scope.headerLogoImg+new Date().getTime();;
+                                
+                            }
+                            else
+                            {
+                                $dialog.alert(file.name + ' upload failed.','Error','pficon pficon-error-circle-o');
+                            }
+                        });
+                    }
                 }
             }
         }
         else if(file !=undefined)
         {
-            $dialog.alert('Only '+$scope.allowedExtList +' file format supported ','Error','pficon pficon-error-circle-o');
+            $dialog.alert('Only '+$scope.allowedExtList +' file format supported.','Error','pficon pficon-error-circle-o');
         }
     };
     
