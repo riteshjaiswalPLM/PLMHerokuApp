ee0a8687c9d3f0987d9d19892308110604102f79
sonam.patel <sonam.patel@LTSU016.sailfin.com>
2018-04-16 17:14:38 +0530

    commit - admin side tab config for bulk upload tab

--- public/slds/src/modules/admin/bulkcreationconfig/services.js
+++ public/slds/src/modules/admin/bulkcreationconfig/services.js
@@ -6,7 +6,7 @@ admin.factory('CSVUploadConfigService', ['$http', function ($http) {
             return $http.post('/api/admin/bulkuploadconfig/getfieldmapping');
         },
         saveFieldMapping: function (config) {
-            angular.forEach(config.config, function (field, index) {
+            angular.forEach(config.configs, function (field, index) {
                 if (field.SObjectField !== undefined && field.SObjectField !== null) {
                     delete field.SObjectField;
                 }
